ead1bc7a360d6873297e3f6089fc08250c9f5073
Vudiepk3 <110141392+Vudiepk3@users.noreply.github.com>
2024-11-25 10:49:25 +0700

    update new code

--- app/src/main/java/com/example/translatorappmlkit/MainActivity.java
+++ app/src/main/java/com/example/translatorappmlkit/MainActivity.java
@@ -1,16 +1,14 @@
 package com.example.translatorappmlkit;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.mlkit.common.model.DownloadConditions;
 import com.google.mlkit.nl.translate.TranslateLanguage;
 import com.google.mlkit.nl.translate.Translation;
@@ -48,43 +46,32 @@ public class MainActivity extends AppCompatActivity {
 
         // Tải mô hình nếu cần thiết
         englishGermanTranslator.downloadModelIfNeeded(conditions)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
+                .addOnSuccessListener(aVoid ->{
                         // Khi mô hình đã tải thành công, thiết lập sự kiện click cho nút dịch
                         translateButton.setOnClickListener(v -> {
                             String input = inputEditText.getText().toString(); // Lấy văn bản đầu vào
                             translateText(input, outputText); // Gọi phương thức dịch
-                        });
-                    }
+
+                    });
                 })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
+                .addOnFailureListener(e ->{
                         // Hiển thị thông báo lỗi nếu tải mô hình thất bại
                         Toast.makeText(MainActivity.this, "Model download failed", Toast.LENGTH_SHORT).show();
-                    }
+
                 });
     }
 
     // Phương thức để dịch văn bản
+    @SuppressLint("SetTextI18n")
     public void translateText(String inputText, TextView outputText) {
-        // Gọi phương thức dịch của Translator
+        // Gọi phương thức dịch của trình dịch
         englishGermanTranslator.translate(inputText)
-                .addOnSuccessListener(new OnSuccessListener<String>() {
-                    @Override
-                    public void onSuccess(String translatedText) {
-                        // Cập nhật văn bản đã dịch lên TextView
-                        outputText.setText(translatedText);
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        // Hiển thị thông báo lỗi nếu dịch thất bại
-                        outputText.setText("Error");
-                        Toast.makeText(MainActivity.this, "Translation failed", Toast.LENGTH_SHORT).show();
-                    }
+                .addOnSuccessListener(outputText::setText) // Sử dụng method reference để cập nhật TextView
+                .addOnFailureListener(e -> {
+                    // Hiển thị thông báo lỗi nếu dịch thất bại
+
+                    outputText.setText("Error");
+                    Toast.makeText(MainActivity.this, "Dịch Thuật Bị Lỗi", Toast.LENGTH_SHORT).show();
                 });
     }
 }
